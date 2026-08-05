ae9d43b1d2d70554dcbd2cc521ed5b7f798c0c10
Eaglie <stefan.eggli@me.com>
2026-08-05 22:05:11 +0200

    Update streamlit_app.py

--- SFL_Automation/streamlit_app.py
+++ SFL_Automation/streamlit_app.py
@@ -1,9 +1,10 @@
 import streamlit as st
 
-st.set_page_config(
-    page_title="Matchblatt",
-    layout="wide"
-)
+from sfl import lade_sfl
+from transfermarkt import lade_transfermarkt
+from report import erstelle_report
+
+st.set_page_config(page_title="Matchblatt", layout="wide")
 
 st.title("MATCHBLATT")
 
@@ -11,4 +12,20 @@ sfl_url = st.text_input("SFL Matchcenter URL")
 heim_url = st.text_input("Transfermarkt Heim")
 gast_url = st.text_input("Transfermarkt Gast")
 
-st.button("MATCHBLATT ERSTELLEN")
+if st.button("MATCHBLATT ERSTELLEN"):
+
+    st.write("Lade SFL...")
+    sfl = lade_sfl(sfl_url)
+
+    st.write("Lade Heim...")
+    heim = lade_transfermarkt(heim_url, sfl["heim"])
+
+    st.write("Lade Gast...")
+    gast = lade_transfermarkt(gast_url, sfl["gast"])
+
+    heim["letzter_gegner"] = sfl["gast"]
+    gast["letzter_gegner"] = sfl["heim"]
+
+    erstelle_report(sfl, gast, heim)
+
+    st.success("Matchblatt erstellt!")
