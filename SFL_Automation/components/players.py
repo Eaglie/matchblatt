d3839d34c93047e9f481b81fca2e267f0732903f
Eaglie <stefan.eggli@me.com>
2026-08-11 07:53:43 +0200

    Update players.py

--- SFL_Automation/components/players.py
+++ SFL_Automation/components/players.py
@@ -14,9 +14,9 @@ def players(spieler):
         html += f"""
 <div class="player" style="left:{x}%;top:{y}%">
 
-    <div class="player_number" style="background:#000000; color:#ffffff; border-radius:50%; width:32px; height:32px; display:flex; align-items:center; justify-content:center;">{nummer}</div>
+    <div class="player_circle" style="background:#000000; color:#ffffff; border-radius:50%; width:36px; height:36px; display:flex; align-items:center; justify-content:center; font-size:15px; font-weight:700;">{nummer}</div>
 
-    <div class="player_name">{name}</div>
+<div class="player_name">{name}</div>
 
     <div class="player_position">{position}</div>
 
